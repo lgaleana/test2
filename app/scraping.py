91d36c0e18343daee49f0e758475987f747495e4
lgaleana-llm <155101820+lgaleana-llm@users.noreply.github.com>
2024-05-20 00:12:17 -0400

    Limit number of images to scrape using N_IMAGES environment variable (#19)
    * Limit number of images to scrape using N_IMAGES environment variable
    
    * Limit number of images to scrape using N_IMAGES environment variable

--- app/scraping.py
+++ app/scraping.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
+import os
+
 
 def extract_images(soup: BeautifulSoup) -> list:
     images = []
+    n_images = int(os.getenv("N_IMAGES", 4))  # Default to 4 if not set
     for img in soup.find_all(["img", "meta", "link", "source"]):
         if img.name == "img" and img.get("src"):
             images.append(img["src"])
@@ -11,4 +14,6 @@ def extract_images(soup: BeautifulSoup) -> list:
             images.append(img["href"])
         elif img.name == "source" and img.get("srcset"):
             images.append(img["srcset"].split()[0])  # Take the first URL in srcset
+        if len(images) >= n_images:
+            break
     return images
