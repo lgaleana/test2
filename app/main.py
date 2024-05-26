817f332992350fc047cb03bd8c2318ca9ad60c4f
lgaleana-LLM <lsgaleana+llm@gmail.com>
2024-05-26 22:23:43 +0000

    Address comment

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.routes import app as routes_app
 app = FastAPI()
 
 # Mount the static files directory
-app.mount("/static", StaticFiles(directory="templates/static"), name="static")
+app.mount("/static", StaticFiles(directory="app/templates/static"), name="static")
 
 # Include the routes
 app.include_router(routes_app)
